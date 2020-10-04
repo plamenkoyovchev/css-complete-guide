9f7e8a7cd0c0b29a9b816945c988f007adc2f875
Plamen Yovchev <plamenkooo@gmail.com>
2020-10-04 10:40:09 +0300

    Show/Hide side nav

--- shared.js
+++ shared.js
@@ -2,14 +2,28 @@ const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('.modal');
 const selectPlanButtons = document.querySelectorAll('.plan button');
 const noButton = document.querySelector('.modal__action--negative');
+const hamburgerButton = document.querySelector('.toggle-button');
+const sideNav = document.querySelector('.mobile-nav');
 
 const attachEventHandlers = () => {
-    backdrop.addEventListener('click', closeModal);
+    backdrop.addEventListener('click', function () {
+        closeModal();
+        hideSideNavigation();
+    });
     noButton.addEventListener('click', closeModal);
-
     selectPlanButtons.forEach(button => {
         button.addEventListener('click', showModal);
     });
+    hamburgerButton.addEventListener('click', showSidebarNavigation);
+};
+
+const showSidebarNavigation = () => {
+    sideNav.style.display = 'block';
+    backdrop.style.display = 'block';
+};
+
+const hideSideNavigation = () => {
+    sideNav.style.display = 'none';
 };
 
 const closeModal = () => {
